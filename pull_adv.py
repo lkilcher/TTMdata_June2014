73feb9d23a765e4fbe4fdf672fd0694f9046f5ce
Levi Kilcher <Levi.Kilcher@nrel.gov>
2017-04-30 11:55:44 -0600

    Add a hash check

--- pull_adv.py
+++ pull_adv.py
@@ -6,42 +6,64 @@ except:
 
 import os.path as path
 import shutil
+import hashlib
 
-
-class Finf(object):
-
-    def __init__(self, fname, url, size):
-        self.fname = fname
-        self.url = url
-        self.size = size
-
-mhkdr = 'https://mhkdr.openei.org/files/'
-# {fname: (url, filesize)}
-FILEINFO = [
-    Finf('ADV/ttm01_ADVtop_NREL02_June2014.vec',
-         mhkdr + '50/ttm01_ADVtop_NREL02_June2014.vec',
-         247559612),
-    Finf('ADV/ttm01_ADVbot_NREL01_June2014.vec',
-         mhkdr + '50/ttm01_ADVbottom_NREL01_June2014.vec',
-         247977218),
-    Finf('ADV/ttm01b_ADVtop_NREL02_June2014.vec',
-         mhkdr + '50/ttm01b_ADVtop_NREL02_June2014.vec',
-         321388010),
-    Finf('ADV/ttm01b_ADVbot_NREL01_June2014.vec',
-         mhkdr + '50/ttm01b_ADVbottom_NREL01_June2014.vec',
-         327756366),
-    Finf('ADV/ttm02b_ADVtop_NREL03_June2014.vec',
-         mhkdr + '50/ttm02b_ADVtop_NREL03_June2014.vec',
-         318368731),
-    Finf('ADV/ttm02b_ADVbot_F01_June2014.vec',
-         mhkdr + '50/ttm02b_ADVbottom_F01_June2014.vec',
-         316418997),
-]
 
 try:
     thisdir = path.dirname(path.realpath(__file__))
 except NameError:
     thisdir = './'
+
+
+def sha(fname):
+    h = hashlib.sha256()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(65536), b""):
+            h.update(chunk)
+    return h.hexdigest()[:16]
+
+
+class Finf(object):
+
+    def __init__(self, fname, url, size, hash=None):
+        self.fname = fname
+        self.url = url
+        self.size = size
+        self.hash = hash
+
+    def checkhash(self, ):
+        if self.hash is None:
+            return True
+        return self.hash == sha(self.fname)
+
+
+mhkdr = 'https://mhkdr.openei.org/files/'
+# (fname, url, filesize, hash)
+FILEINFO = [
+    Finf('ADV/ttm01_ADVtop_NREL02_June2014.vec',
+         mhkdr + '50/ttm01_ADVtop_NREL02_June2014.vec',
+         247559612, 'df5d15e92c6c2d5b'),
+
+    Finf('ADV/ttm01_ADVbot_NREL01_June2014.vec',
+         mhkdr + '50/ttm01_ADVbottom_NREL01_June2014.vec',
+         247977218, 'c1fd5448b155ea7f'),
+
+    Finf('ADV/ttm01b_ADVtop_NREL02_June2014.vec',
+         mhkdr + '50/ttm01b_ADVtop_NREL02_June2014.vec',
+         321388010, 'c639b458ae3055a2'),
+
+    Finf('ADV/ttm01b_ADVbot_NREL01_June2014.vec',
+         mhkdr + '50/ttm01b_ADVbottom_NREL01_June2014.vec',
+         327756366, '3117987dd5840e58'),
+
+    Finf('ADV/ttm02b_ADVtop_NREL03_June2014.vec',
+         mhkdr + '50/ttm02b_ADVtop_NREL03_June2014.vec',
+         318368731, '5801641c827d5991'),
+
+    Finf('ADV/ttm02b_ADVbot_F01_June2014.vec',
+         mhkdr + '50/ttm02b_ADVbottom_F01_June2014.vec',
+         316418997, 'b77e4040ab77b4e3'),
+]
 
 
 def checkfile(finf, ):
@@ -51,22 +73,32 @@ def checkfile(finf, ):
         print("Size of local file '{}' is wrong. Redownloading..."
               .format(finf.fname))
         return False
-    print("File '{}' already exists.".format(finf.fname))
+    if not finf.checkhash():
+        print("Secure hash of local file '{}' is wrong. Redownloading..."
+              .format(finf.fname))
+        return False
     return True
 
 
-def retrieve(finf, show_progress=True):
+def retrieve(finf):
     response = urlopen(finf.url)
-    with open(thisdir + '/' + finf.name, 'wb') as f:
+    with open(thisdir + '/' + finf.fname, 'wb') as f:
         shutil.copyfileobj(response, f)
 
 
-def main():
+def main(test_only=False):
     for finf in FILEINFO:
         if not checkfile(finf):
+            if test_only:
+                continue
             print("Downloading '{}'... ".format(finf.fname), end='')
-            retrieve(finf.url, finf.fname)
+            retrieve(finf)
+            if not finf.checkhash():
+                raise Exception('Secure hash check failed!')
             print("Done.")
+        else:
+            print("File '{}' already exists.".format(finf.fname))
+
 
 if __name__ == '__main__':
     main()
