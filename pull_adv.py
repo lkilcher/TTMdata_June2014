8a0d93af231bce058ddff77c1bb433ae14be6412
Levi Kilcher <Levi.Kilcher@nrel.gov>
2017-04-30 10:51:08 -0600

    Add a filesize check.

--- pull_adv.py
+++ pull_adv.py
@@ -7,6 +7,7 @@ except:
 import os.path as path
 import shutil
 
+
 URLS = [
     'https://mhkdr.openei.org/files/50/ttm01_ADVtop_NREL02_June2014.vec',
     'https://mhkdr.openei.org/files/50/ttm01_ADVbottom_NREL01_June2014.vec',
@@ -15,11 +16,29 @@ URLS = [
     'https://mhkdr.openei.org/files/50/ttm02b_ADVtop_NREL03_June2014.vec',
     'https://mhkdr.openei.org/files/50/ttm02b_ADVbottom_F01_June2014.vec',
 ]
+sizes = {
+    'ttm01_ADVtop_NREL02_June2014.vec': 247559612,
+    'ttm01_ADVbot_NREL01_June2014.vec': 247977218,
+    'ttm01b_ADVtop_NREL02_June2014.vec': 321388010,
+    'ttm01b_ADVbot_NREL01_June2014.vec': 327756366,
+    'ttm02b_ADVtop_NREL03_June2014.vec': 318368731,
+    'ttm02b_ADVbot_F01_June2014.vec': 316418997,
+]
 
 try:
     thisdir = path.dirname(path.realpath(__file__))
 except NameError:
     thisdir = './'
+
+
+def checkfile(fname, ):
+    if not path.isfile(fname):
+        return False
+    if not path.getsize(fname) == sizes[fname]:
+        print("Size of {} is wrong. Redownloading...".format(fname))
+        return False
+    print("File '{}' already exists.".format(fname))
+    return True
 
 
 def retrieve(url, name, show_progress=True):
@@ -31,12 +50,10 @@ def retrieve(url, name, show_progress=True):
 def main():
     for url in URLS:
         fname = 'ADV/' + url.rsplit('/', 1)[-1].replace('bottom', 'bot')
-        if not path.isfile(fname):
+        if not checkfile:
             print("Downloading '{}'... ".format(fname), end='')
             retrieve(url, fname)
             print("Done.")
-        else:
-            print("File '{}' already exists.".format(fname))
 
 if __name__ == '__main__':
     main()
